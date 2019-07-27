630343b5186678f23da713ec05c08d9b8b01b511
anang <anangnov99@gmail.com>
2019-07-28 03:12:19 +0700

    main

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
     <div id="app">
-        <b-navbar toggleable="sm" class="pl-5 pr-5" type="light" variant="light" >
+        <b-navbar toggleable="sm" class="pl-5 pr-5 nav-white shadow p-2">
             <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
             <b-collapse id="nav-text-collapse" is-nav>
-                <b-navbar-brand>Home</b-navbar-brand>
+                <!-- <b-navbar-brand><i class="fas fa-home"></i></b-navbar-brand> -->
                 <b-navbar-nav>
-                    <b-nav-item><router-link to="/">Jadwal Sholat</router-link></b-nav-item>
-                    <b-nav-item><router-link to="/quran">Jadwal Sholat</router-link></b-nav-item>
+                    <b-nav-item><router-link to="/"><i class="fas fa-home"></i> Home</router-link></b-nav-item>
+                    <b-nav-item><router-link to="/"><i class="fas fa-bell"></i> Jadwal Sholat</router-link></b-nav-item>
+                    <b-nav-item><router-link to="/quran"><i class="fas fa-book-open"></i> Quran</router-link></b-nav-item>
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="container-custom pt-4 pb-4">
+    <div class="container-custom pt-5 pb-4">
         <div class="row">
             <div class="col-md-4">
                 <b-card
                     title="Card Title"
-                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-src="https://via.placeholder.com/250x150"
                     img-alt="Image"
                     img-top
                     tag="article"
                     style="max-width: 20rem;"
-                    class="mb-2"
+                    class="shadow-lg mb-2"
                 >
                     <b-card-text>
                     Some quick example text to build on the card title and make up the bulk of the card's content.
@@ -17,7 +17,6 @@
 
                     <b-button href="#" variant="primary">Go somewhere</b-button>
                 </b-card>
-                <i class="fas fa-user"></i>
             </div>
         </div>
     </div>
